c7fd5720d6e8a4a2f7d5ba9bb65454edcaa1d00b
Gergø Lonyai <gergely@lonyai.com>
2025-09-26 12:08:45 +0200

    Certbot update

--- func/certbot-deploy-hook.sh
+++ func/certbot-deploy-hook.sh
@@ -6,6 +6,7 @@ CERTBOT_USER="$(/bin/ls -d /home/*/web/"$CERTBOT_DOMAIN" | awk -F / '{print $3}'
 
 ssl_dir="$VESTA/data/users/${CERTBOT_USER}/ssl"
 ssl_conf=/etc/letsencrypt/renewal/${CERTBOT_DOMAIN}.conf
+env
 /bin/cat $(crudini --get ${ssl_conf} DEFAULT privkey) > $ssl_dir/${CERTBOT_DOMAIN}.key
 /bin/cat $(crudini --get ${ssl_conf} DEFAULT cert) > $ssl_dir/${CERTBOT_DOMAIN}.crt
 /bin/cat $(crudini --get ${ssl_conf} DEFAULT chain) > $ssl_dir/${CERTBOT_DOMAIN}.ca
