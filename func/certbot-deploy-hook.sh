e65dd2c3337651f2f9aa61f5e7873e5856aaa906
Gergø Lonyai <gergely@lonyai.com>
2025-09-29 11:55:38 +0200

    Update certbot deploy hook script

--- func/certbot-deploy-hook.sh
+++ func/certbot-deploy-hook.sh
@@ -2,18 +2,19 @@
 #
 . /etc/profile.d/vesta.sh
 
+[[ -n "$CERTBOT_DOMAIN" ]] || CERTBOT_DOMAIN="${RENEWED_DOMAINS%% *}"
+[[ -n "$CERTBOT_DOMAIN" ]] || (echo "Undefined RENEWED_DOMAINS"; env ; exit 1)
+[[ -n "$RENEWED_LINEAGE" ]] || (echo "Undefined RENEWED_LINEAGE"; env ; exit 1)
 CERTBOT_USER="$(/bin/ls -d /home/*/web/"$CERTBOT_DOMAIN" | awk -F / '{print $3}')"
 
 ssl_dir="$VESTA/data/users/${CERTBOT_USER}/ssl"
-ssl_conf=/etc/letsencrypt/renewal/${CERTBOT_DOMAIN}.conf
-env
-/bin/cat $(crudini --get ${ssl_conf} DEFAULT privkey) > $ssl_dir/${CERTBOT_DOMAIN}.key
-/bin/cat $(crudini --get ${ssl_conf} DEFAULT cert) > $ssl_dir/${CERTBOT_DOMAIN}.crt
-/bin/cat $(crudini --get ${ssl_conf} DEFAULT chain) > $ssl_dir/${CERTBOT_DOMAIN}.ca
-/bin/cat $(crudini --get ${ssl_conf} DEFAULT fullchain) > $ssl_dir/${CERTBOT_DOMAIN}.pem
+/bin/cat "${RENEWED_LINEAGE}/privkey.pem" > "$ssl_dir/${CERTBOT_DOMAIN}.key"
+/bin/cat "${RENEWED_LINEAGE}/cert.pem" > "$ssl_dir/${CERTBOT_DOMAIN}.crt"
+/bin/cat "${RENEWED_LINEAGE}/chain.pem" > "$ssl_dir/${CERTBOT_DOMAIN}.ca"
+/bin/cat "${RENEWED_LINEAGE}/fullchain.pem" > "$ssl_dir/${CERTBOT_DOMAIN}.pem"
 
 for I in key crt ca pem ; do
-  cp -f  "$ssl_dir/${CERTBOT_DOMAIN}.${I}" /home/$user/conf/web/ssl.${CERTBOT_DOMAIN}.${I}
+  cp -f  "$ssl_dir/${CERTBOT_DOMAIN}.${I}" "/home/$CERTBOT_USER/conf/web/ssl.${CERTBOT_DOMAIN}.${I}"
 done
 
 /bin/systemctl reload nginx.service
