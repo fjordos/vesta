ea95c6a2e75423847ec6db1ca60fb62f482eaef8
Gergø Lonyai <gergely@lonyai.com>
2025-10-09 14:28:55 +0200

    The first "smart" update trying

--- install/vst-update-rhel10.sh
+++ install/vst-update-rhel10.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ -n "${VESTA}" ]] ; then
+  source /etc/profile.d/vesta.sh
+  source "${VESTA}/conf/vesta.conf"
+fi
 DEBUG=1
 
 /bin/rsync ${DEBUG:+-v} -aH "${VESTA}/install/rhel/10/letsencrypt/" "/etc/letsencrypt"
