a0a0fb805627550f543dd146da08c39cab5319e8
Gergø Lonyai <gergely@lonyai.com>
2025-10-14 16:28:21 +0200

    Next fix of certbot process

--- install/rhel/10/letsencrypt/renewal-hooks/pre/authenticator.sh
+++ install/rhel/10/letsencrypt/renewal-hooks/pre/authenticator.sh
@@ -5,8 +5,8 @@ DEBUG=""
 ${DEBUG:+set -x}
 ${DEBUG:+env}
 
-RENEWED_USER="$(/bin/grep -E "(DOMAIN='|ALIAS='|,)${RENEWED_DOMAIN}[,']" "${VESTA}/data/users/*/web.conf" | awk -F / '{print $7}')" || (echo "User not found for $RENEWED_DOMAIN"; env ; exit 1)
-CERTBOT_DOMAIN_BASE=$(/bin/grep -E "(DOMAIN='|ALIAS='|,)${RENEWED_DOMAIN}[,']" "${VESTA}/data/users/${RENEWED_USER}/web.conf" | awk -F "'" '{print $2}')
+RENEWED_USER="$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${RENEWED_DOMAIN}[,']" "${VESTA}/data/users/*/web.conf" | awk -F / '{print $7}')" || (echo "User not found for $RENEWED_DOMAIN"; env ; exit 1)
+CERTBOT_DOMAIN_BASE=$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${RENEWED_DOMAIN}[,']" "${VESTA}/data/users/${RENEWED_USER}/web.conf" | awk -F "'" '{print $2}')
 
 cat > "/home/${RENEWED_USER}/conf/web/nginx.${CERTBOT_DOMAIN_BASE}.conf_letsencrypt_${CERTBOT_DOMAIN}" << EOF
 location "/.well-known/acme-challenge/$CERTBOT_TOKEN" {
