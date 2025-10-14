13b6f8f07ff6e52684c22850375c63a956f58e4f
Gergø Lonyai <gergely@lonyai.com>
2025-10-14 16:28:21 +0200

    Next fix of certbot process

--- install/rhel/10/letsencrypt/renewal-hooks/post/cleanup.sh
+++ install/rhel/10/letsencrypt/renewal-hooks/post/cleanup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-DEBUG=""
-. /etc/profile.d/vesta.sh
-${DEBUG:+set -x}
-${DEBUG:+env}
+if [[ -n "${CERTBOT_DOMAIN}" ]] ; then
+  DEBUG=""
+  . /etc/profile.d/vesta.sh
+  ${DEBUG:+set -x}
+  ${DEBUG:+env}
 
-RENEWED_USER="$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${RENEWED_DOMAIN}[,']" "${VESTA}/data/users/*/web.conf" | awk -F / '{print $7}')" || (echo "User not found for $RENEWED_DOMAIN"; env ; exit 1)
-CERTBOT_DOMAIN_BASE=$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${RENEWED_DOMAIN}[,']" "${VESTA}/data/users/${RENEWED_USER}/web.conf" | awk -F "'" '{print $2}')
+  CERTBOT_USER="$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}/data/users/*/web.conf" | awk -F / '{print $7}')" || (echo "User not found for $CERTBOT_DOMAIN"; env ; exit 1)
+  CERTBOT_DOMAIN_BASE=$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}/data/users/${CERTBOT_USER}/web.conf" | awk -F "'" '{print $2}')
 
-rm -f ${DEBUG:+-v} "/home/${RENEWED_USER}/conf/web/nginx.${CERTBOT_DOMAIN_BASE}.conf_letsencrypt_*"
+  rm -f ${DEBUG:+-v} "/home/${CERTBOT_USER}/conf/web/{s,}nginx.${CERTBOT_DOMAIN_BASE}.conf_letsencrypt_${CERTBOT_DOMAIN}"
+else
+  echo "CERTBOT_DOMAIN not set, skipping..."
+fi
--- install/rhel/10/letsencrypt/renewal-hooks/pre/authenticator.sh
+++ install/rhel/10/letsencrypt/renewal-hooks/pre/authenticator.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-DEBUG=""
-. /etc/profile.d/vesta.sh
-${DEBUG:+set -x}
-${DEBUG:+env}
+if [[ -n "${CERTBOT_DOMAIN}" ]] ; then
+  DEBUG=""
+  . /etc/profile.d/vesta.sh
+  ${DEBUG:+set -x}
+  ${DEBUG:+env}
 
-RENEWED_USER="$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${RENEWED_DOMAIN}[,']" "${VESTA}/data/users/*/web.conf" | awk -F / '{print $7}')" || (echo "User not found for $RENEWED_DOMAIN"; env ; exit 1)
-CERTBOT_DOMAIN_BASE=$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${RENEWED_DOMAIN}[,']" "${VESTA}/data/users/${RENEWED_USER}/web.conf" | awk -F "'" '{print $2}')
+  CERTBOT_USER="$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}/data/users/*/web.conf" | awk -F / '{print $7}')" || (echo "User not found for $CERTBOT_DOMAIN"; env ; exit 1)
+  CERTBOT_DOMAIN_BASE=$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}/data/users/${CERTBOT_USER}/web.conf" | awk -F "'" '{print $2}')
 
-cat > "/home/${RENEWED_USER}/conf/web/nginx.${CERTBOT_DOMAIN_BASE}.conf_letsencrypt_${CERTBOT_DOMAIN}" << EOF
+  for I in nginx snginx ; do
+    cat > "/home/${CERTBOT_USER}/conf/web/${I}.${CERTBOT_DOMAIN_BASE}.conf_letsencrypt_${CERTBOT_DOMAIN}" << EOF
 location "/.well-known/acme-challenge/$CERTBOT_TOKEN" {
     default_type text/plain;
     return 200 "$CERTBOT_VALIDATION";
 }
 EOF
-
-nginx -t && nginx -s reload
+  done
+  nginx -t && nginx -s reload
+else
+  echo "CERTBOT_DOMAIN not set, skipping..."
+fi
