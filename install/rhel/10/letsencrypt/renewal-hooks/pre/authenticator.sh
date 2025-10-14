2846a470a749a77f9fd7aa72f175dbb8f1cdfa2c
Gergø Lonyai <gergely@lonyai.com>
2025-10-14 16:28:21 +0200

    Next fix of certbot process

--- install/rhel/10/letsencrypt/renewal-hooks/pre/authenticator.sh
+++ install/rhel/10/letsencrypt/renewal-hooks/pre/authenticator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [[ -n "${CERTBOT_DOMAIN}" ]] ; then
   DEBUG=""
@@ -6,7 +6,7 @@ if [[ -n "${CERTBOT_DOMAIN}" ]] ; then
   ${DEBUG:+set -x}
   ${DEBUG:+env}
 
-  CERTBOT_USER="$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}/data/users/*/web.conf" | awk -F / '{print $7}')" || (echo "User not found for $CERTBOT_DOMAIN"; env ; exit 1)
+  CERTBOT_USER="$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}"/data/users/*/web.conf | awk -F / '{print $7}')"
   CERTBOT_DOMAIN_BASE=$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}/data/users/${CERTBOT_USER}/web.conf" | awk -F "'" '{print $2}')
 
   for I in nginx snginx ; do
