958324e5e6d47a55a11c3894f531d5b15ed3c429
Gergø Lonyai <gergely@lonyai.com>
2025-10-17 21:26:08 +0200

    Update letsencrypt hooks

--- install/rhel/10/letsencrypt/renewal-hooks/pre/authenticator.sh
+++ install/rhel/10/letsencrypt/renewal-hooks/pre/authenticator.sh
@@ -19,13 +19,12 @@ EOF
   done
   nginx -t  || { echo "Failed to reload Nginx" ; exit 1; }
   nginx -s reload
-  sleep "${S:=1}"s
   until curl --fail --silent --show-error --max-time 5 --write-out '%{http_code}' --output /dev/null "http://${CERTBOT_DOMAIN_BASE}/.well-known/acme-challenge/${CERTBOT_TOKEN}" | grep -q '^200$' ; do
     if ((S>5)) ; then
       echo "Failed to obtain certificate for ${CERTBOT_DOMAIN_BASE}"
       exit 1
     fi
-    sleep "${S}"s
+    sleep "${S:=1}"s
     S=$((S+1))
   done
 else
