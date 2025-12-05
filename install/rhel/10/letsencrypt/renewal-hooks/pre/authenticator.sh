fd94306e444987a6eb50abf35840f3c446d4a2ca
Gergø Lonyai <gergely@lonyai.com>
2025-12-05 16:25:19 +0100

    Polish the letsencrypt

--- install/rhel/10/letsencrypt/renewal-hooks/pre/authenticator.sh
+++ install/rhel/10/letsencrypt/renewal-hooks/pre/authenticator.sh
@@ -9,6 +9,10 @@ if [[ -n "${CERTBOT_DOMAIN}" ]] ; then
   CERTBOT_USER="$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}"/data/users/*/web.conf | awk -F / '{print $7}')"
   CERTBOT_DOMAIN_BASE=$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}/data/users/${CERTBOT_USER}/web.conf" | awk -F "'" '{print $2}')
 
+  if [[ ! -f "/home/${CERTBOT_USER}/conf/web/${CERTBOT_DOMAIN_BASE}.nginx.ssl.conf" ]] ; then
+      sed -i '/return 301 https:/d' "/home/${CERTBOT_USER}/conf/web/${CERTBOT_DOMAIN_BASE}.nginx.conf"
+  fi
+
   for I in nginx snginx ; do
     cat > "/home/${CERTBOT_USER}/conf/web/${I}.${CERTBOT_DOMAIN_BASE}.conf_letsencrypt_${CERTBOT_DOMAIN}" << EOF
 location "/.well-known/acme-challenge/$CERTBOT_TOKEN" {
