d42fedd5ed9a1ad5c18d94220f7f7d99ddb5b864
Gergø Lonyai <gergely@lonyai.com>
2025-12-18 12:16:33 +0100

    Set VESTA_DEBUG to default false

--- web/inc/constants.php
+++ web/inc/constants.php
@@ -6,6 +6,6 @@ define('JQUERY_COOKIE_VERSION', '1.4.1');
 define('JQUERY_UI_VERSION', '1.14.1');
 //define('POPPER_VERSION', '1.16.1');
 
-defined('VESTA_DEBUG') || define('VESTA_DEBUG', "true");
+defined('VESTA_DEBUG') || define('VESTA_DEBUG', "false");
 defined('VESTA_CMD') || define('VESTA_CMD', '/usr/bin/sudo /usr/local/vesta/bin/');
 defined('VESTA_VERSION') || define('VESTA_VERSION', 'stable');
