f0e6b42def464ac45a4ae911cb68ea6e6fa0b611
Gergø Lonyai <gergely@lonyai.com>
2025-12-30 20:02:27 +0100

    Test a Let's Encrypt cleanup

--- install/rhel/10/letsencrypt/renewal-hooks/post/cleanup.sh
+++ install/rhel/10/letsencrypt/renewal-hooks/post/cleanup.sh
@@ -8,8 +8,11 @@ if [[ -n "${CERTBOT_DOMAIN}" ]] ; then
 
   CERTBOT_USER="$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}"/data/users/*/web.conf | awk -F / '{print $7}')"
   CERTBOT_DOMAIN_BASE=$(/bin/grep -E "(DOMAIN='| ALIAS='|,)${CERTBOT_DOMAIN}[,']" "${VESTA}/data/users/${CERTBOT_USER}/web.conf" | awk -F "'" '{print $2}')
-
-  rm -f ${DEBUG:+-v} "/home/${CERTBOT_USER}/conf/web/{s,}nginx.${CERTBOT_DOMAIN_BASE}.conf_letsencrypt_${CERTBOT_DOMAIN}" ||:
+  if [[ "$CERTBOT_USER" ]] || [[ "$CERTBOT_DOMAIN_BASE" ]]; then
+    rm -f ${DEBUG:+-v} "/home/${CERTBOT_USER}/conf/web/{s,}nginx.${CERTBOT_DOMAIN_BASE}.conf_letsencrypt_${CERTBOT_DOMAIN}"
+  else
+    certbot delete -d "${CERTBOT_DOMAIN}"
+  fi
 else
   echo "CERTBOT_DOMAIN not set, skipping..."
 fi
